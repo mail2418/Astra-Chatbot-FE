2eaf5f93a0376ab3a324ffc25e85059b5612088a
mail2418 <muhammadismail2418@gmail.com>
2024-05-27 13:43:29 +0700

    feat:add deploy script

--- scripts/cloud_run_deploy.sh
+++ scripts/cloud_run_deploy.sh
@@ -3,21 +3,16 @@ cd "$(dirname "$0")"
 
 echo $(dirname "$0")
 # Load environment variables from .env
-export $(grep -v '^#' .env | xargs)
+export $(grep -v '^#' ../.env | xargs)
 
 # Prepare environment variables for the gcloud command
-ENV_VARS=$(grep -v '^#' .env | grep -v '^GOOGLE_APPLICATION_CREDENTIALS=' | xargs | sed 's/ /,/g')
+ENV_VARS=$(grep -v '^#' ../.env | grep -v '^GOOGLE_APPLICATION_CREDENTIALS=' | xargs | sed 's/ /,/g')
 
 echo $ENV_VARS
 
-# Build the Docker image
-gcloud builds submit \
-  --region=asia-southeast2 \
-  --tag docker.pkg.dev/$GCP_PROJECT_ID/$GCP_CLOUD_BUILD_IMAGE_TAG \
-
-# Deploy to Cloud Run
+# Deploy to Cloud Run using Docker Hub
 gcloud run deploy $GCP_CLOUD_RUN_DEPLOY_ID \
-  --image docker.pkg.dev/$GCP_PROJECT_ID/$GCP_CLOUD_BUILD_IMAGE_TAG \
+  --image docker.io/$GCP_PROJECT_ID/$GCP_CLOUD_BUILD_IMAGE_TAG \
   --region $GCP_REGION \
   --service-account $GCP_SERVICE_ACCOUNT_EMAIL \
   --allow-unauthenticated \
